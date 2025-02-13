7f1f1a101911f20e62367a7f03fba4d3ce885d1e
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2025-02-13 20:33:32 +0545

    files added

---
+++ lab2.asm
@@ -0,0 +1,17 @@
+;Assembly level Program to toggle case in 8086
+
+.model small
+.stack 64   
+.data
+.code
+    main proc far 
+        
+        mov ax, @data
+        mov ds, ax
+            
+      
+            
+    main endp 
+end
+
+
---
+++ no_of_word.asm
@@ -0,0 +1,64 @@
+;Assembly level Program to change into uppercase and print each character in newline in 8086
+
+.model small
+.stack 64   
+.data
+    MAXLEN DB 255
+    ACTLEN DB ?
+    INPUT DB 255 DUP('$')   
+    
+    NEW_LINE MACRO
+        mov ah, 02h
+        mov dl, 0ah
+        int 21h 
+                
+        mov dl, 0dh
+        int 21h      
+        
+    NEW_LINE ENDM 
+    
+.code
+    main proc far 
+        
+        MOV ax, @data
+        MOV ds, ax
+        
+     
+        MOV AH, 0AH
+        LEA DX, MAXLEN
+        INT 21H 
+        
+        ;CALL SCR_CLEAR 
+        
+        
+        NEW_LINE
+        MOV CX, 0000H  
+        MOV CL, ACTLEN
+        
+        MOV BL, 01H; WORD COUNT
+        L1:
+            CMP AL, 32;SPACE
+            JNZ SKIP_COUNT
+            INC BX
+            
+            SKIP_COUNT:
+            INC SI
+            
+            
+        
+        LOOP L1  
+                 
+        ADD BL, 48         
+        MOV DL, BL        
+        MOV AH, 02h; FOR DISPLAY
+        INT 21H  
+        NEW_LINE
+
+      
+            
+    main endp
+  
+         
+ end
+
+
